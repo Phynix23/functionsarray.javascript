bf74169ef4d73aa929c382f8494d3712c58a5289
Abuoma David <davidabuoma4@gmail.com>
2026-03-27 05:54:21 +0100

    second and fourth - array length

---
+++ 2nd4thmonth.js
@@ -0,0 +1,7 @@
+function printSpecificMonths() {
+  const months = ["January", "February", "March", "April", "May"];
+  console.log("Second month:", months[1]);
+  console.log("fourth month:", months[3]);
+  console.log("third month:", months[2]);
+}
+printSpecificMonths();
---
+++ arraylength.js
@@ -0,0 +1,5 @@
+function printVotesCounts() {
+  const vote = [3, 7, 5, 9, 6];
+  console.log("Total number of items:", vote.length)
+}
+printVotesCounts();
