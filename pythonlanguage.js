6bcf06c7e1b97590b9cbc3aeb41e8edad6f60d6c
Abuoma David <davidabuoma4@gmail.com>
2026-03-27 06:08:51 +0100

    naira expenses

--- pythonlanguage.js
+++ pythonlanguage.js
@@ -1,6 +1,6 @@
 function checkPythonInLanguages() {
   const languages = ["Javascript", "Python", "Java", "C++"];
-  const hasPython = languages.includes("Python");
+  const hasPython = languages.includes("python");
   console.log(hasPython);
   return hasPython;
 }
