cfb7b75d59ace79a706d4f7d79df3cc24284e49b
Abuoma David <davidabuoma4@gmail.com>
2026-03-27 06:02:49 +0100

    python check

---
+++ pythonlanguage.js
@@ -0,0 +1,7 @@
+function checkPythonInLanguages() {
+  const languages = ["Javascript", "Python", "Java", "C++"];
+  const hasPython = languages.includes("Python");
+  console.log(hasPython);
+  return hasPython;
+}
+checkPythonInLanguages();
