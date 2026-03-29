332585100c5084dbb4a7f9197aed84db8f483ffb
Abuoma David <davidabuoma4@gmail.com>
2026-03-29 12:05:25 +0100

    subjects with longer name

---
+++ Subjectwithlongernames.js
@@ -0,0 +1,9 @@
+function printLongSubjectNames() {
+    const subjects = ["Maths", "English", "Science", "History", "Civic"];
+    for (let i = 0; i < subjects.length; i++) {
+        if (subjects[i].length > 5) {
+            console.log(subjects[i]);
+        }
+    }
+}
+printLongSubjectNames();
